37cf53ffdace8c7e648e5a2cd3dc17bb382b8532
Daniel Witurna <daniel.witurna@gmail.com>
2022-05-31 10:40:44 +0200

    Initial Commit

---
+++ ResizableEmbeddedView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ResizableEmbeddedView
+//
+//  Created by Daniel Witurna on 31.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ResizableEmbeddedView/ResizableEmbeddedViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResizableEmbeddedViewApp.swift
+//  ResizableEmbeddedView
+//
+//  Created by Daniel Witurna on 31.05.22.
+//
+
+import SwiftUI
+
+@main
+struct ResizableEmbeddedViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
